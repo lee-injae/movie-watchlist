69c83f81fb72c79e4850cd41877fbe14335a1a45
Injae Lee <ilee.injae@gmail.com>
2023-07-13 23:51:33 -0400

    fixed one script two html rendering issues

--- index.js
+++ index.js
@@ -1,37 +1,26 @@
 const url = "https://www.omdbapi.com/?apikey=36ea6bc0"
 
-
 const movieContainerClass = document.querySelector(".movies-container")
-
 const moviesEl = document.getElementById("movies")
 const searchInputEl = document.getElementById("search-input")
 const plusBtnEl = document.getElementById("plus-btn")
+
 let watchlist = []
 let currentlyAddingId = "" 
 
-document.addEventListener("click", function(e){
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("loaded")
+    render()
+})
+
+document.addEventListener("click", e => {
     if (e.target.id === "search-btn"){
         e.preventDefault()
         let searchInput = searchInputEl.value
+        showLoading()
         getMovieCard(searchInput)
         searchInput = ""
     }
-
-    else if (e.target.classList.contains("minus-btn")){
-        let removingMovie = e.target.dataset.movieId
-        console.log("removing movie: ", removingMovie)
-        console.log("dataset: ", e.target.dataset)
-        let removingIndex = watchlist.indexOf(removingMovie)
-        if (removingIndex !== -1){
-            watchlist.splice(removingIndex, 1) //update watchlist array
-            localStorage.setItem("watchlistArray", 
-                JSON.stringify(watchlist)) //update localstorage
-        }
-        console.log(watchlist)
-        console.log("localstorage after remove", localStorage)
-        moviesEl.innerHTML = ""
-        renderWatchlist()
-        }
 
     else if (e.target.classList.contains("plus-btn")){
         let targetMovieID = e.target.dataset.movie
@@ -41,99 +30,54 @@ document.addEventListener("click", function(e){
         : addMovieToWatchlist(targetMovieID)
         // let findDuplicates = watchlist => watchlist.filter(
         //     movieID => (movieID === targetMovieID))
-        console.log(localStorage)
+        console.log("localstorage", localStorage)
+        console.log("watchlist", watchlist)
     }
 
-    // else if (e.target.id === "watchlist-link"){
-    //     console.log("hey")
-    //     renderWatchlist()
-    // }
+    else if (e.target.classList.contains("minus-btn")){
+        let removingMovie = e.target.dataset.movieId
+        removeMovieFromWatchlist(removingMovie)
+    }
 })
 
-renderWatchlist()
-
-
-function addMovieToWatchlist(movieStr){
-    watchlist.push(movieStr)
-    localStorage.setItem("watchlistArray", JSON.stringify(watchlist))
+function render(){
+    let page = document.body.id
+    console.log(page)
+    switch (page) {
+        case "search":
+            getMovieCard()
+            console.log(localStorage)
+            break
+        case "watchlist":
+            getWatchlist()
+            break
+        // default:
+        //     somethingElse()
+    }
 }
-
-function renderWatchlist(){
-    let localStorageWatchlist = JSON.parse(localStorage.getItem("watchlistArray")) || []
-
-    localStorageWatchlist.forEach(function(movieId){
-        let movieCardHtml = ""
-        let htmlArr = []    
-
-        fetch(`${url}&i=${movieId}`)
-            .then(res => res.json())
-            .then(data => {        
-                movieCardHtml = `
-                <div class="movie-card">
-                    <img class="movie-poster"src="${data.Poster}"/>
-                    <div class="movie-title">
-                        <h2>${data.Title}</h2>
-                        <img src="assets/star-icon.png" alt="star-icon"/>
-                        <h4>${data.imdbRating}</h4>
-                    </div>
-                    <div class="movie-detail">
-                        <h4>${data.Runtime}</h4>
-                        <h4>${data.Genre}</h4>
-                        <h4 class="watchlist">
-                            <img class="plus-btn" 
-                                src="./assets/plus-icon.png"
-                                data-movie='${data.imdbID}'
-                            />
-                            <div>Watchlist</div>
-                        </h4>
-                    </div>
-                    <p class="movie-plot">${data.Plot}</p>   
-                </div>
-                `
-                htmlArr.push(movieCardHtml)
-                console.log("htmlarr-before promise ", htmlArr)    
-            })
-            .then(() => {
-                console.log("htmlarr-after promise ", htmlArr)    
-
-                moviesEl.innerHTML = htmlArr.join('')
-            })
-        })
-
-}
-
 
 function getMovieCard(searchString){
-    fetch(`${url}&t=${searchString}`)
+    if (searchInputEl.value) {
+        fetch(`${url}&t=${searchString}`)
         .then(res => res.json())
         .then(data => {
             if (data.Response === "False") {
+                hidePlaceholder()
                 renderErrMsg()
-                hideFilmIcon()
                 throw Error("Move not Found!!")
             }
-            renderMovieCardHtml(data)
+            hidePlaceholder()
+            showSearchedMovieHtml(data)
         })
         .catch(err => {
             console.error((err))
         })
+    }     
 }
 
-function renderErrMsg() {
-    let errMsgHtml = `
-        <div class="error-smg strong">
-            <h2>Unable to find what you're looking for. Please try another search.</h2>
-        </div>
-    `
-    moviesEl.innerHTML = errMsgHtml
-}
-
-function renderMovieCardHtml(movieObj){
-    hideFilmIcon()
+function showSearchedMovieHtml(movieObj){
     moviesEl.innerHTML = ""
     let movieCardHtml = ""
-    console.log(movieObj)
-
     movieCardHtml = `
         <div class="movie-card">
             <img class="movie-poster"src="${movieObj.Poster}"/>
@@ -155,34 +99,98 @@ function renderMovieCardHtml(movieObj){
             </div>
             <p class="movie-plot">${movieObj.Plot}</p>   
         </div>
-            
         `
     moviesEl.innerHTML += movieCardHtml
 }
 
-// document.addEventListener("click", function(e){
-//     if (e.target.classList.contains("plus-btn")){
-//         let movieID = e.target.dataset.movie
-//         console.log("dataset: ", movieID)
-//         watchlist.push(movieID)
-//         console.log("watchlist: ", watchlist)
-    
-//         localStorage.setItem("watchListArray", JSON.stringify(watchlist))
-//     }
-// })
+function getWatchlist(){
 
-function hideFilmIcon(){
+    
+
+    let localStorageWatchlist = JSON.parse(
+        localStorage.getItem("watchlistArray")) 
+        || []
+
+    console.log("localstoragewatchlist", localStorageWatchlist) 
+
+    if (localStorageWatchlist.length > 0) { 
+        hidePlaceholder()
+        showLoading()
+        let movieCardHtml = ""
+        localStorageWatchlist.forEach( movieId => { 
+        fetch(`${url}&i=${movieId}`)
+            .then(res => res.json())
+            .then(data => {        
+                showWatchlistHtml(movieCardHtml, data)
+            })
+        })
+    }
+}
+
+function showWatchlistHtml(htmlStr, movieObj){
+    const {Poster, Title, imdbRating, imdbID, Genre, Runtime, Plot} = movieObj
+    moviesEl.innerHTML = ""
+    htmlStr = `
+                    <div class="movie-card">
+                        <img class="movie-poster"src="${Poster}"/>
+                        <div class="movie-title">
+                            <h2>${Title}</h2>
+                            <img src="assets/star-icon.png" alt="star-icon"/>
+                            <h4>${imdbRating}</h4>
+                        </div>
+                        <div class="movie-detail">
+                            <h4>${Runtime}</h4>
+                            <h4>${Genre}</h4>
+                            <h4 class="watchlist">
+                                <img class="minus-btn" 
+                                    src="./assets/minus-icon.png"
+                                    data-movie='${imdbID}'
+                                />
+                                <div>Watchlist</div>
+                            </h4>
+                        </div>
+                        <p class="movie-plot">${Plot}</p>   
+                    </div>
+                `
+    return moviesEl.innerHTML += htmlStr
+}
+
+function removeMovieFromWatchlist(movieIdStr){
+    console.log("removing movie: ", movieIdStr)
+    let removingIndex = watchlist.indexOf(removingMovie)
+    if (removingIndex !== -1){
+        watchlist.splice(removingIndex, 1) //update watchlist array
+        localStorage.setItem("watchlistArray", 
+            JSON.stringify(watchlist)) //update localstorage
+    }
+    console.log(watchlist)
+    console.log("localstorage after remove", localStorage)
+}
+
+function addMovieToWatchlist(movieStr){
+    watchlist.push(movieStr)
+    localStorage.setItem("watchlistArray", JSON.stringify(watchlist))
+}
+
+function renderErrMsg() {
+    let errMsgHtml = `
+        <div class="error-msg strong">
+            <h2>Unable to find what you're looking for. Please try another search.</h2>
+        </div>
+    `
+    moviesEl.innerHTML = errMsgHtml
+}
+
+function hidePlaceholder(){
     document.getElementById("placeholder").classList.add("hide")
 }
 
 function showLoading(){
+    console.log("loading")
     let h3 = document.createElement("h3")
     h3.textContent = "Loading..."
-    
+    h3.className = "loading-list"
+    moviesEl.appendChild(h3)
 }
 
-let ul = document.querySelector('.list');
-let li = document.createElement('li');
-li.textContent = 'Loading...';
-li.className = 'loading-list';
-ul.appendChild(li);
+
